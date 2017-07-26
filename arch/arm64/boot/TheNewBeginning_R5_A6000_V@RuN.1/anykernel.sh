3ad3ce040950462f457d608ea8738d3304e29fca
MRTHAKER <mahek.haker@gmail.com>
2017-07-26 09:26:56 +0000

    first commit

---
+++ arch/arm64/boot/TheNewBeginning_R5_A6000_V@RuN.1/anykernel.sh
@@ -0,0 +1,48 @@
+# AnyKernel2 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# EDIFY properties
+kernel.string=TheNewBeginning By TheHardGamer_V@RuN
+kernel.release=R5 Jul 13 2017
+do.devicecheck=0
+do.initd=1
+do.modules=0
+do.cleanup=1
+device.name1=
+device.name2=
+device.name3=
+device.name4=
+device.name5=
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. /tmp/anykernel/tools/ak2-core.sh;
+
+
+## AnyKernel permissions
+# set permissions for included ramdisk files
+chmod -R 755 $ramdisk
+chmod 644 $ramdisk/sbin/media_profiles.xml
+
+
+## AnyKernel install
+dump_boot;
+
+# begin ramdisk changes
+
+# init.rc
+backup_file init.rc;
+insert_line init.rc "import /init.spectrum.rc" after "import /init.trace.rc" "import /init.spectrum.rc\n";
+
+# end ramdisk changes
+
+write_boot;
+
+## end install
+
---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 12 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#12 SMP PREEMPT Wed Jul 26 08:06:15 UTC 2017"
+#define LINUX_COMPILE_BY "ubuntu"
+#define LINUX_COMPILE_HOST "optimusprime"
+#define LINUX_COMPILER "gcc version 7.1.1 20170707 (Linaro GCC 7.1-2017.07-1~dev) "
